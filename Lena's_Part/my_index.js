863afc32b9f44e40387ce100ab2cef8f7e722b0d
Kyltism <vkyrsaxvalyt@mail.ru>
2022-04-18 20:01:33 +0300

    dfgh

--- Lena's_Part/my_index.js
+++ Lena's_Part/my_index.js
@@ -2,18 +2,6 @@ let goods = document.getElementById('goods');
 
 let pictureNames = document.getElementById('pictureNames');
 
-function createDiv(className) {
-    let imgSrc = document.createElement('div');
-    imgSrc.className = className;
-    imgSrc.id = 'img';
-    return imgSrc;
-}
-
-
-for (var i = 0; i < 10; i++) {
-    goods.append(createDiv('img'));
-}
-let imgOfGoods = document.getElementById('img')
 
 let goodsCard = [];
 fetch('https://624dcdf853326d0cfe52fb09.mockapi.io/cards/cards')
@@ -24,18 +12,43 @@ fetch('https://624dcdf853326d0cfe52fb09.mockapi.io/cards/cards')
     .then((goodsCard) => {
 
         goodsCard.forEach(item => {
+            const div = document.createElement('div');
+            div.className = 'container';
+
+            const priceGoods = document.createElement('div');
+            priceGoods.className = 'priceGoods';
+
             const img = document.createElement('img');
             img.className = 'cardImg';
             img.src = item.src;
             img.id = item.id;
-            imgOfGoods.append(img);
+            div.appendChild(img);
 
-            const span = document.createElement('span');
-            span.innerHTML = item.name;
-            imgOfGoods.append(span);
+            const price = document.createElement('p');
+            price.innerHTML = item.price;
+            price.className = 'pricing';
+            priceGoods.appendChild(price)
+
+            const oldPrice = document.createElement('p');
+            oldPrice.innerHTML = item.OldPrice;
+            oldPrice.className = 'oldPrice';
+            priceGoods.appendChild(oldPrice);
+
+
+            const p = document.createElement('p');
+            p.innerHTML = item.name;
+            p.className = 'nameGoods'
+            div.appendChild(p);
+
+            div.appendChild(priceGoods)
+
+            goods.append(div);
+            
+
         })
     })
 
+ 
 
 
 
