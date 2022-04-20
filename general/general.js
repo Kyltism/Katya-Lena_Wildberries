9b2834b109a03e7cec96a04c580b762741d57097
Kyltism <vkyrsaxvalyt@mail.ru>
2022-04-20 20:48:37 +0300

    ывапт

--- general/general.js
+++ general/general.js
@@ -76,6 +76,7 @@ let goods = document.getElementById('goods');
 
 let pictureNames = document.getElementById('pictureNames');
 
+let fastButton = '';
 
 let goodsCard = [];
 fetch('https://624dcdf853326d0cfe52fb09.mockapi.io/cards/cards')
@@ -93,8 +94,10 @@ fetch('https://624dcdf853326d0cfe52fb09.mockapi.io/cards/cards')
             fastView.className = 'button';
             div.appendChild(fastView);
 
-            const fastButton = document.createElement('button');
+            fastButton = document.createElement('button');
             fastButton.className = 'fastButton';
+            fastButton.innerHTML = 'Быстрый просмотр';
+            fastButton.id = 'fastButton';
             fastView.appendChild(fastButton);
 
             const priceGoods = document.createElement('div');
@@ -102,9 +105,16 @@ fetch('https://624dcdf853326d0cfe52fb09.mockapi.io/cards/cards')
 
             const img = document.createElement('img');
             img.className = 'cardImg';
+            img.setAttribute("id", "cardImg");
+            img.className = 'cardImg'
             img.src = item.src;
             img.id = item.id;
             fastView.appendChild(img);
+
+            const basket = document.createElement('button');
+            basket.className = 'basket';
+            basket.innerHTML = '<img src = "./img/basket.jpg" class = "basketImg">'
+            fastView.appendChild(basket);
 
             const price = document.createElement('p');
             price.innerHTML = item.price;
@@ -117,15 +127,38 @@ fetch('https://624dcdf853326d0cfe52fb09.mockapi.io/cards/cards')
             priceGoods.appendChild(oldPrice);
 
             div.appendChild(priceGoods)
-            
+
 
             const p = document.createElement('p');
             p.innerHTML = item.name;
             p.className = 'nameGoods'
             div.appendChild(p);
 
-            
+
             goods.append(div);
 
         })
     })
+    .then(() => {
+        //получаем идентификатор элемента
+        let but = [...document.getElementsByClassName('fastButton')];
+
+        but.forEach((item) => {
+            ;
+            let img = item.nextSibling;
+                        //вешаем на него событие
+            item.onclick = () => {
+                //производим какие-то действия
+                if (img.style.width == '240px' && img.style.height == '300px');
+                else {
+                    img.style.width = '500px';
+                    img.style.height = '560px';
+                }
+
+            }
+        })
+
+
+    });
+
+
